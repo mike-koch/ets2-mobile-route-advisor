8d5f8e093eb4ea5be559813f221bddb42df54095
Mike Koch <mkoch227@gmail.com>
2015-05-24 23:38:34 -0400

    Start including new telemetry features
    Still need to test though

--- dashboard.js
+++ dashboard.js
@@ -12,30 +12,39 @@
 
 Funbit.Ets.Telemetry.Dashboard.prototype.filter = function (data) {
     // round truck speed
-    data.truckSpeedRounded = Math.abs(data.truckSpeed > 0
-        ? Math.floor(data.truckSpeed)
-        : Math.round(data.truckSpeed));
+    data.truckSpeedRounded = Math.abs(data.truck.speed > 0
+        ? Math.floor(data.truck.speed)
+        : Math.round(data.truck.speed));
     // convert kilometers per hour to miles per hour (just an example)
-    data.truckSpeedMph = data.truckSpeed * 0.621371;
+    data.truckSpeedMph = data.truck.speed * 0.621371;
     data.truckSpeedMphRounded = Math.abs(Math.floor(data.truckSpeedMph));
-    // format odometer data as: 00000.0
-    data.truckOdometer = (Math.round(data.truckOdometer * 10) / 10).toFixed(1);
     // convert gear to readable format
-    data.gear = data.gear > 0 ? 'D' + data.gear : (data.gear < 0 ? 'R' : 'N');
-    // convert rpm to rpm * 100
-    data.engineRpm = data.engineRpm / 100;
-    data.currentFuelPercentage = (data.fuel / data.fuelCapacity) * 100;
+    data.gear = data.truck.gear > 0 ? 'D' + data.truck.gear : (data.truck.gear < 0 ? 'R' : 'N');
+    data.currentFuelPercentage = (data.truck.fuel / data.truck.fuelCapacity) * 100;
     
     // scsTruckDamage is the value SCS uses in the route advisor
     data.scsTruckDamage = getDamagePercentage(data);
     data.scsTruckDamageRounded = Math.floor(data.scsTruckDamage);
-    data.wearTrailerRounded = Math.floor(data.wearTrailer * 100);
+    data.wearTrailerRounded = Math.floor(data.trailer.wear * 100);
     
-    data.gameTime12h = getTimeInTwelveHourFormat(data.gameTime);
-    data.jobDeadlineTime12h = getTimeInTwelveHourFormat(data.jobDeadlineTime);
-    data.trailerMassTons = data.hasJob ? ((data.trailerMass / 1000.0) + ' t') : '';
-    data.trailerMassKg = data.hasJob ? data.trailerMass + ' kg' : '';
-    data.jobIncome = getJobIncome(data.jobIncome);
+    data.gameTime12h = getTime(data.game.time, 12, true);
+    data.jobDeadlineTime12h = getTime(data.job.deadlineTime, 12, false);
+    data.trailerMassTons = data.trailer.attached ? ((data.trailer.mass / 1000.0) + ' t') : '';
+    data.trailerMassKg = data.trailer.attached ? data.trailer.mass + ' kg' : '';
+    data.jobIncome = getJobIncome(data.job.income);
+    data.game.nextRestStopTimeArray = getTimeDifference(data.game.time, data.game.nextRestStopTime);
+    data.game.nextRestStopTime = processTimeDifferenceArray(data.game.nextRestStopTimeArray);
+    data.navigation.speedLimitMph = data.navigation.speedLimit * .621371;
+    data.navigation.speedLimitMphRounded = Math.floor(data.navigation.speedLimitMph);
+    data.navigation.estimatedDistanceKm = data.navigation.estimatedDistance / 1000;
+    data.navigation.estimatedDistanceMi = data.navigation.estimatedDistanceKm * .621371;
+    data.navigation.estimatedDistanceKmRounded = Math.floor(data.navigation.estimatedDistanceKm);
+    data.navigation.estimatedDistanceMiRounded = Math.floor(data.navigation.estimatedDistanceMi);
+    var originalEstimatedTime = data.navigation.estimatedTime;
+    data.navigation.estimatedTime = getTime(data.navigation.estimatedTime, 24, false);
+    data.navigation.estimatedTime12h = getTime(originalEstimatedTime, 12, false);
+    var timeToDestinationArray = getTimeDifference(data.game.time, originalEstimatedTime);
+    data.navigation.timeToDestination = processTimeDifferenceArray(timeToDestinationArray);
     
     // return changed data to the core for rendering
     return data;
@@ -48,7 +57,23 @@ Funbit.Ets.Telemetry.Dashboard.prototype.render = function (data) {
     $('#fuelLine').css('width', data.currentFuelPercentage + '%');
     $('#damageLine').css('width', data.scsTruckDamage + '%');
     $('#truckDamageIcon').css('height', getDamageFillForTruck(data.scsTruckDamage) + '%');
-    $('#trailerDamageIcon').css('height', getDamageFillForTrailer(data.wearTrailer * 100) + '%');
+    $('#trailerDamageIcon').css('height', getDamageFillForTrailer(data.trailer.wear * 100) + '%');
+    $('#restLine').css('width', getFatiguePercentage(data.game.nextRestStopTimeArray[0], data.game.nextRestStopTimeArray[1]) + '%');
+    
+    // Process DOM for connection
+    if (data.game.connected) {
+        $('.has-connection').show();
+        $('.no-connection').hide();
+    }
+    
+    // Process DOM for job
+    if (data.trailer.attached) {
+        $('.hasJob').show();
+        $('.noJob').hide();
+    } else {
+        $('.hasJob').hide();
+        $('.noJob').show();
+    }
 }
 
 Funbit.Ets.Telemetry.Dashboard.prototype.initialize = function (skinConfig) {
@@ -62,10 +87,16 @@ Funbit.Ets.Telemetry.Dashboard.prototype.initialize = function (skinConfig) {
     var distanceUnits = skinConfig.distanceUnits;
     if (distanceUnits === 'km') {
         $('.speedUnits').text('km/h');
+        $('.distanceUnits').text('km');
         $('.truckSpeedRoundedKmhMph').addClass('truckSpeedRounded').removeClass('truckSpeedRoundedKmhMph');
+        $('.speedLimitRoundedKmhMph').addClass('navigation-speedLimit').removeClass('speedLimitRoundedKmhMph');
+        $('.navigationEstimatedDistanceKmMi').addClass('navigation-estimatedDistanceKmRounded').removeClass('navigationEstimatedDistanceKmMi');
     } else if (distanceUnits === 'mi') {
         $('.speedUnits').text('mph');
+        $('.distanceUnits').text('mi');
         $('.truckSpeedRoundedKmhMph').addClass('truckSpeedMphRounded').removeClass('truckSpeedRoundedKmhMph');
+        $('.speedLimitRoundedKmhMph').addClass('navigation-speedLimitMphRounded').removeClass('speedLimitRoundedKmhMph');
+        $('.navigationEstimatedDistanceKmMi').addClass('navigation-estimatedDistanceMiRounded').removeClass('navigationEstimatedDistanceKmMi');
     }
     
     // Process kg vs tons
@@ -79,8 +110,9 @@ Funbit.Ets.Telemetry.Dashboard.prototype.initialize = function (skinConfig) {
     // Process 12 vs 24 hr time
     var timeFormat = skinConfig.timeFormat;
     if (timeFormat === '12h') {
-        $('.gameTime').addClass('gameTime12h').removeClass('gameTime');
-        $('.jobDeadlineTime').addClass('jobDeadlineTime12h').removeClass('jobDeadlineTime');
+        $('.game-time').addClass('gameTime12h').removeClass('game-time');
+        $('.job-deadlineTime').addClass('jobDeadlineTime12h').removeClass('job-deadlineTime');
+        $('.navigation-estimatedTime').addClass('navigation-estimatedTime12h').removeClass('navigation-estimatedTime');
     }
     
     // Process currency code
@@ -93,10 +125,49 @@ Funbit.Ets.Telemetry.Dashboard.prototype.initialize = function (skinConfig) {
         });
     });
 }
+
+function getFatiguePercentage(hoursUntilRest, minutesUntilRest) {
+    var FULLY_RESTED_TIME_REMAINING_IN_MILLISECONDS = 11*60*60*1000; // 11 hours * 60 min * 60 sec * 1000 milliseconds
     
-function getTimeInTwelveHourFormat(gameTime) {
+    if (hoursUntilRest <= 0 && minutesUntilRest <= 0) {
+        return 100;
+    }
+    
+    var hoursInMilliseconds = hoursUntilRest * 60 * 60 * 1000; // # hours * 60 min * 60 sec * 1000 milliseconds
+    var minutesInMilliseconds = minutesUntilRest * 60 * 1000; // # minutes * 60 sec * 1000 milliseconds
+    
+    return 100 - (((hoursInMilliseconds + minutesInMilliseconds) / FULLY_RESTED_TIME_REMAINING_IN_MILLISECONDS) * 100);
+}
+
+function processTimeDifferenceArray(hourMinuteArray) {
+    var hours = hourMinuteArray[0];
+    var minutes = hourMinuteArray[1];
+    
+    
+    if (hours <= 0 && minutes <= 0) {
+        minutes = $('.lXMinutes').text().replace('{0}', 0);
+        return minutes;
+    }
+    
+    if (hours == 1) {
+        hours = $('.lXHour').text().replace('{0}', hours);
+    } else if (hours == 0) {
+        hours = '';
+    } else {
+        hours = $('.lXHours').text().replace('{0}', hours);
+    }
+    
+    if (minutes == 1) {
+        minutes = $('.lXMinute').text().replace('{0}', minutes);
+    } else {
+        minutes = $('.lXMinutes').text().replace('{0}', minutes); 
+    }
+    return hours + ' ' + minutes;
+}
+    
+function getTime(gameTime, timeUnits, isHeader) {
     var currentTime = new Date(gameTime);
-    var currentPeriod = ' AM';
+    var currentPeriod = timeUnits === 12 ? ' AM' : '';
     var currentHours = currentTime.getUTCHours();
     var currentMinutes = currentTime.getUTCMinutes();
     var formattedMinutes = currentMinutes < 10 ? '0'+currentMinutes : currentMinutes;
@@ -126,13 +197,13 @@ function getTimeInTwelveHourFormat(gameTime) {
             break;
     }
     
-    if (currentHours > 12) {
+    if (currentHours > 12 && timeUnits === 12) {
         currentHours -= 12;
         currentPeriod = ' PM';
     }
     var formattedHours = currentHours < 10 ? '0'+currentHours : currentHours;
     
-    if (currentDay == 'Wednesday') {
+    if (currentDay == 'Wednesday' && isHeader) {
         $('#headerTime').css('font-size','.9em');
     } else {
         $('#headerTime').css('font-size','1em');
@@ -179,11 +250,11 @@ function getJobIncome(income) {
 
 function getDamagePercentage(data) {
     // Return the max value of all damage percentages.
-    return Math.max(data.wearEngine, 
-                    data.wearTransmission, 
-                    data.wearCabin, 
-                    data.wearChassis, 
-                    data.wearWheels) * 100;
+    return Math.max(data.truck.wearEngine, 
+                    data.truck.wearTransmission, 
+                    data.truck.wearCabin, 
+                    data.truck.wearChassis, 
+                    data.truck.wearWheels) * 100;
 }
 
 function getDamageFillForTruck(damagePercentage) {
@@ -230,4 +301,17 @@ function showTab(tabName) {
     // Show the ID requested
     $('#' + tabName).show();
     $('#' + tabName + 'Footer').addClass('_footerSelected');
+}
+
+/** Returns the difference between two dates in ISO 8601 format in an [hour, minutes] array */
+function getTimeDifference(begin, end) {
+    var beginDate = new Date(begin);
+    var endDate = new Date(end);
+    var MILLISECONDS_IN_MINUTE = 60*1000;
+    var MILLISECONDS_IN_HOUR = MILLISECONDS_IN_MINUTE*60;
+    var MILLISECONDS_IN_DAY = MILLISECONDS_IN_HOUR*24;
+    
+    var hours = Math.floor((endDate - beginDate) % MILLISECONDS_IN_DAY / MILLISECONDS_IN_HOUR) // number of hours
+    var minutes = Math.floor((endDate - beginDate) % MILLISECONDS_IN_DAY % MILLISECONDS_IN_HOUR / MILLISECONDS_IN_MINUTE) // number of minutes
+    return [hours, minutes];
 }
