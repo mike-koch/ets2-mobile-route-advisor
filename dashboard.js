188e7a0ecd6e843ebf03ae98f49776ab1bd00f2a
Mike Koch <mkoch227@gmail.com>
2016-02-07 22:01:48 -0500

    Bump version

--- dashboard.js
+++ dashboard.js
@@ -458,7 +458,7 @@ var g_translations;
 var g_skinConfig;
 
 // The current version of ets2-mobile-route-advisor
-var g_currentVersion = '3.1.0';
+var g_currentVersion = '3.2.0';
 
 // The currently running game
 var g_runningGame;
