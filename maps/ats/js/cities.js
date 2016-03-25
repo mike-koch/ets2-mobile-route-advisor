5383a47cf3f85083eba97b0d3aa6447e135fd2c6
Koen <koen@koenvh.nl>
2016-03-25 13:33:10 +0100

    Added ATS map pack

---
+++ maps/ats/js/cities.js
@@ -0,0 +1,242 @@
+var g_cities_json = [
+    {
+      "gameName": "bakersfield",
+      "realName": "Bakersfield",
+      "country": "california",
+      "x": "-52261.9",
+      "y": "61.457",
+      "z": "20598.8"
+    },
+    {
+      "gameName": "barstow",
+      "realName": "Barstow",
+      "country": "california",
+      "x": "-47300.4",
+      "y": "100.59",
+      "z": "21963.2"
+    },
+    {
+      "gameName": "carlsbad",
+      "realName": "Carlsbad",
+      "country": "california",
+      "x": "-49106.5",
+      "y": "61.4336",
+      "z": "27008"
+    },
+    {
+      "gameName": "carson_city",
+      "realName": "Carson City",
+      "country": "nevada",
+      "x": "-51957.3",
+      "y": "127.836",
+      "z": "8904.93"
+    },
+    {
+      "gameName": "elko",
+      "realName": "Elko",
+      "country": "nevada",
+      "x": "-45027.1",
+      "y": "82.2227",
+      "z": "2043.13"
+    },
+    {
+      "gameName": "ely",
+      "realName": "Ely",
+      "country": "nevada",
+      "x": "-43077.7",
+      "y": "122.629",
+      "z": "7074.14"
+    },
+    {
+      "gameName": "el_centro",
+      "realName": "El Centro",
+      "country": "california",
+      "x": "-41183.4",
+      "y": "41.6445",
+      "z": "29223.9"
+    },
+    {
+      "gameName": "eureka",
+      "realName": "Eureka",
+      "country": "california",
+      "x": "-68616.5",
+      "y": "87.7422",
+      "z": "3021.09"
+    },
+    {
+      "gameName": "fresno",
+      "realName": "Fresno",
+      "country": "california",
+      "x": "-54802.6",
+      "y": "72.7031",
+      "z": "16248.6"
+    },
+    {
+      "gameName": "hornbrook",
+      "realName": "Hornbrook",
+      "country": "california",
+      "x": "-63040.5",
+      "y": "134.578",
+      "z": "-2368.46"
+    },
+    {
+      "gameName": "huron",
+      "realName": "Huron",
+      "country": "california",
+      "x": "-56245.7",
+      "y": "105.332",
+      "z": "18908.2"
+    },
+    {
+      "gameName": "jackpot",
+      "realName": "Jackpot",
+      "country": "nevada",
+      "x": "-41684.1",
+      "y": "130.223",
+      "z": "-1865.5"
+    },
+    {
+      "gameName": "las_vegas",
+      "realName": "Las Vegas",
+      "country": "nevada",
+      "x": "-41596.9",
+      "y": "209.461",
+      "z": "17626.8"
+    },
+    {
+      "gameName": "los_angeles",
+      "realName": "Los Angeles",
+      "country": "california",
+      "x": "-52693.3",
+      "y": "87.7422",
+      "z": "24704.3"
+    },
+    {
+      "gameName": "oakdale",
+      "realName": "Oakdale",
+      "country": "california",
+      "x": "-56869.8",
+      "y": "87.7422",
+      "z": "14073.5"
+    },
+    {
+      "gameName": "oakland",
+      "realName": "Oakland",
+      "country": "california",
+      "x": "-58786.3",
+      "y": "87.7422",
+      "z": "14301.8"
+    },
+    {
+      "gameName": "oxnard",
+      "realName": "Oxnard",
+      "country": "california",
+      "x": "-56628.7",
+      "y": "87.7422",
+      "z": "21492.7"
+    },
+    {
+      "gameName": "pioche",
+      "realName": "Pioche",
+      "country": "nevada",
+      "x": "-40938.4",
+      "y": "137.867",
+      "z": "10214.7"
+    },
+    {
+      "gameName": "primm",
+      "realName": "Primm",
+      "country": "nevada",
+      "x": "-43011.8",
+      "y": "94.3945",
+      "z": "20256.2"
+    },
+    {
+      "gameName": "redding",
+      "realName": "Redding",
+      "country": "california",
+      "x": "-61340",
+      "y": "87.7422",
+      "z": "2201.14"
+    },
+    {
+      "gameName": "reno",
+      "realName": "Reno",
+      "country": "nevada",
+      "x": "-55425.1",
+      "y": "55.7656",
+      "z": "5836.53"
+    },
+    {
+      "gameName": "sacramento",
+      "realName": "Sacramento",
+      "country": "california",
+      "x": "-59012.1",
+      "y": "87.7422",
+      "z": "10440.7"
+    },
+    {
+      "gameName": "santa_cruz",
+      "realName": "Santa Cruz",
+      "country": "california",
+      "x": "-58791.1",
+      "y": "87.7422",
+      "z": "18772.1"
+    },
+    {
+      "gameName": "san_diego",
+      "realName": "San Diego",
+      "country": "california",
+      "x": "-46897.8",
+      "y": "87.7422",
+      "z": "29857.3"
+    },
+    {
+      "gameName": "san_francisc",
+      "realName": "San Francisco",
+      "country": "california",
+      "x": "-60374.2",
+      "y": "87.7422",
+      "z": "13271"
+    },
+    {
+      "gameName": "san_rafael",
+      "realName": "San Rafael",
+      "country": "california",
+      "x": "-61918.1",
+      "y": "87.7422",
+      "z": "10079.6"
+    },
+    {
+      "gameName": "stockton",
+      "realName": "Stockton",
+      "country": "california",
+      "x": "-57824.6",
+      "y": "87.7422",
+      "z": "12037.9"
+    },
+    {
+      "gameName": "tonopah",
+      "realName": "Tonopah",
+      "country": "nevada",
+      "x": "-48104.3",
+      "y": "100.676",
+      "z": "12496.8"
+    },
+    {
+      "gameName": "truckee",
+      "realName": "Truckee",
+      "country": "california",
+      "x": "-56640.5",
+      "y": "101.926",
+      "z": "8566.73"
+    },
+    {
+      "gameName": "winnemucca",
+      "realName": "Winnemucca",
+      "country": "nevada",
+      "x": "-50540.7",
+      "y": "94.7539",
+      "z": "1847.23"
+    }
+]
---
+++ maps/ats/js/map.js
@@ -0,0 +1,326 @@
+// All of this should be executed after the DOM is ready and the entire skin has been loaded.
+
+// Image size used in the map.
+var MAX_X = 10840;
+var MAX_Y = 7600;
+// How the image was extracted from the game:
+// http://forum.scssoft.com/viewtopic.php?p=405122#p405122
+
+// Based on http://forum.scssoft.com/viewtopic.php?f=41&t=186779
+function calculatePixelCoordinate(x, y, pointsPerPixel, x0, y0) {
+    return [
+        (x / pointsPerPixel + x0) | 0,
+        (y / pointsPerPixel + y0) | 0
+    ];
+}
+function calculatePixelCoordinateEu(x, y) {
+    return calculatePixelCoordinate(x, y, 7.278, 13164, 16260); //x+16, y+4
+}
+function calculatePixelCoordinateUk(x, y) {
+    //return calculatePixelCoordinate(x, y, 9.69522, 11446, 14422);
+    return calculatePixelCoordinate(x, y, 7.278, 13164, 16260); //x-550, y-1540 //Original pointsPerPixel: 9.69522, own calculated: 8.62552985346, x0: 12093, y0: 15148
+}
+
+function game_coord_to_pixels(x, y) {
+    // http://forum.scssoft.com/viewtopic.php?p=402836#p402836
+    var r = null;
+    //if (x < -31812 && y < -5618 && x > -61000 && y > -67000) {
+    //    r = calculatePixelCoordinateUk(x, y);
+    //} else {
+        r = calculatePixelCoordinateEu(x, y);
+    //}
+
+    // Inverting Y axis, because of OpenLayers coordinates.
+    r[1] = MAX_Y - r[1];
+    return r;
+}
+
+function buildMap(target_element_id){
+    var projection = new ol.proj.Projection({
+        // Any name here. I chose "Funbit" because we are using funbit's image coordinates.
+        code: 'Funbit',
+        units: 'pixels',
+        extent: [0, 0, MAX_X, MAX_Y],
+        worldExtent: [0, 0, MAX_X, MAX_Y]
+    });
+    ol.proj.addProjection(projection);
+
+    // Adding a marker for the player position/rotation.
+    g_playerIcon = g_runningGame === 'ATS' ? new ol.style.Icon({
+        anchor: [0.5, 39],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        rotateWithView: true,
+        src: g_pathPrefix + '/img/player_proportions.png'
+    }) : new ol.style.Icon();
+    var playerIconStyle = new ol.style.Style({
+        image: g_playerIcon
+    });
+    g_playerFeature = new ol.Feature({
+        geometry: new ol.geom.Point([MAX_X / 2, MAX_Y / 2])
+    });
+    // For some reason, we cannot pass the style in the constructor.
+    g_playerFeature.setStyle(playerIconStyle);
+
+    // Adding a layer for features overlaid on the map.
+    var featureSource = new ol.source.Vector({
+        features: [g_playerFeature],
+        wrapX: false
+    });
+    var vectorLayer = new ol.layer.Vector({
+        source: featureSource
+    });
+
+    // Configuring the custom map tiles.
+    var custom_tilegrid = new ol.tilegrid.TileGrid({
+        extent: [0, 0, MAX_X, MAX_Y],
+        minZoom: 0,
+        origin: [0, MAX_Y],
+        tileSize: [256, 256],
+        resolutions: (function(){
+            var r = [];
+            for (var z = 0; z <= 7; ++z) {
+                r[z] = Math.pow(2, 7 - z);
+            }
+            return r;
+        })()
+    });
+
+    // Creating custom controls.
+    var rotate_control = new ol.control.Control({
+        element: document.getElementById('rotate-button-div')
+    });
+    var speed_limit_control = new ol.control.Control({
+        element: document.getElementById('speed-limit')
+    });
+    var text_control = new ol.control.Control({
+        element: document.getElementById('map-text')
+    });
+
+    // Creating the map.
+    g_map = new ol.Map({
+        target: target_element_id,
+        controls: [
+            // new ol.control.ZoomSlider(),
+            // new ol.control.OverviewMap(),
+            // new ol.control.Rotate(),
+            // new ol.control.MousePosition(),  // DEBUG
+            new ol.control.Zoom(),
+            rotate_control,
+            speed_limit_control,
+            text_control
+            // TODO: Set 'tipLabel' on both zoom and rotate controls to language-specific translations.
+        ],
+        interactions: ol.interaction.defaults().extend([
+            // Rotating by using two fingers is implemented in PinchRotate(), which is enabled by default.
+            // With DragRotateAndZoom(), it is possible to use Shift+mouse-drag to rotate the map.
+            // Without it, Shift+mouse-drag creates a rectangle to zoom to an area.
+            new ol.interaction.DragRotateAndZoom()
+        ]),
+        layers: [
+            getMapTilesLayer(projection, custom_tilegrid),
+            getTextLayer(),
+            // Debug layer below.
+            // new ol.layer.Tile({
+            //     extent: [0, 0, MAX_X, MAX_Y],
+            //     source: new ol.source.TileDebug({
+            //         projection: projection,
+            //         tileGrid: custom_tilegrid,
+            //         // tileGrid: ol.tilegrid.createXYZ({
+            //         //  extent: [0, 0, MAX_X, MAX_Y],
+            //         //  minZoom: 0,
+            //         //  maxZoom: 7,
+            //         //  tileSize: [256, 256]
+            //         // }),
+            //         wrapX: false
+            //     })
+            // }),
+            vectorLayer
+        ],
+        view: new ol.View({
+            projection: projection,
+            extent: [0, 0, MAX_X, MAX_Y],
+            //center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
+            center: [MAX_X/2, MAX_Y/2],
+            minZoom: 0,
+            maxZoom: 9,
+            zoom: 7
+        })
+    });
+
+    // Adding behavior to the custom button.
+    var rotate_button = document.getElementById('rotate-button');
+    var rotate_arrow = rotate_button.firstElementChild;
+    g_map.getView().on('change:rotation', function(ev) {
+        rotate_arrow.style.transform = 'rotate(' + ev.target.getRotation() + 'rad)';
+    });
+    rotate_button.addEventListener('click', function(ev) {
+        if (g_behavior_center_on_player) {
+            g_behavior_rotate_with_player = ! g_behavior_rotate_with_player;
+        } else {
+            g_behavior_center_on_player = true;
+        }
+    });
+
+    // Detecting when the user interacts with the map.
+    // https://stackoverflow.com/q/32868671/
+    g_map.getView().on(['change:center', 'change:rotation'], function(ev) {
+        if (g_ignore_view_change_events) {
+            return;
+        }
+
+        // The user has moved or rotated the map.
+        g_behavior_center_on_player = false;
+        // Not needed:
+        // g_behavior_rotate_with_player = false;
+    });
+
+    // Debugging.
+    // map.on('singleclick', function(evt) {
+    //     var coordinate = evt.coordinate;
+    //     console.log(coordinate);
+    // });
+    // map.getView().on('change:center', function(ev) {
+    //   console.log(ev);
+    // });
+    // map.getView().on('change:rotation', function(ev) {
+    //   console.log(ev);
+    // });
+}
+
+function getMapTilesLayer(projection, tileGrid) {
+    if (g_runningGame === 'ATS') {
+        return new ol.layer.Tile({
+            extent: [0, 0, MAX_X, MAX_Y],
+            source: new ol.source.XYZ({
+                projection: projection,
+                url: g_pathPrefix + '/tiles/{z}/{x}/{y}.png',
+                tileSize: [256, 256],
+                // Using createXYZ() makes the vector layer (with the features) unaligned.
+                // It also tries loading non-existent tiles.
+                //
+                // Using custom_tilegrid causes rescaling of all image tiles before drawing
+                // (i.e. no image will be rendered at 1:1 pixels), But fixes all other issues.
+                tileGrid: tileGrid,
+                // tileGrid: ol.tilegrid.createXYZ({
+                //     extent: [0, 0, MAX_X, MAX_Y],
+                //     minZoom: 0,
+                //     maxZoom: 7,
+                //     tileSize: [256, 256]
+                // }),
+                wrapX: false,
+                minZoom: 4,
+                maxZoom: 7
+            })
+        });
+    }
+
+    return new ol.layer.Tile();
+}
+
+var COUNTRY_NAME_TO_CODE = {
+    "california": "ca",
+	"nevada": "nv",
+	"arizona": "az"
+};
+
+// http://codepen.io/denilsonsa/pen/BKWNgB
+function country_code_to_unicode(cc) {
+    cc = cc.toLowerCase();
+    var flagA = 0x1F1E6;
+    var letter_a = 0x61;
+    var a = cc.charCodeAt(0) - letter_a;
+    var b = cc.charCodeAt(1) - letter_a;
+    return String.fromCodePoint(flagA + a, flagA + b);
+}
+
+function getTextFeatures() {
+    var fill = new ol.style.Fill();
+    fill.setColor('#fff');
+    var stroke = new ol.style.Stroke();
+    stroke.setColor('#000');
+    stroke.setWidth(2);
+    var createTextStyle = function(resolution) {
+        var scale = Math.min(1, Math.max(0, 1.0 / Math.log2(resolution + 1) - 0.125));
+        return [new ol.style.Style({
+			//Creating a new image layer
+            image: new ol.style.Icon(({
+                rotateWithView: false,
+                anchor: [0.5, 1],
+                anchorXUnits: 'fraction',
+                anchorYUnits: 'fraction',
+                snapToPixel: false,
+                // Flag images from: http://lipis.github.io/flag-icon-css/
+                src: g_pathPrefix + '/flags-usa/' + this.get('cc') + '.png',
+                scale: 4 / 55 * scale
+            })),
+            text: new ol.style.Text({
+                text: this.get('realName'),
+                font: '32px "Helvetica Neue", "Helvetica", "Arial", sans-serif',
+                textAlign: 'center',
+                fill: fill,
+                stroke: stroke,
+                scale: scale
+            })
+        })];
+    };
+    var features = g_cities_json.map(function(city) {
+        var map_coords = game_coord_to_pixels(city.x, city.z);
+        var feature = new ol.Feature(city);
+        feature.setGeometry(new ol.geom.Point(map_coords));
+        feature.setStyle(createTextStyle);
+        return feature;
+    });
+    return features;
+}
+
+function getTextLayer() {
+    var textSource = new ol.source.Vector({
+        features: getTextFeatures(),
+        wrapX: false
+    });
+    var vectorLayer = new ol.layer.Vector({
+        source: textSource
+    });
+
+    return vectorLayer;
+}
+
+// Global vars.
+var g_map;
+var g_playerFeature;
+var g_playerIcon;
+var g_behavior_center_on_player = true;
+var g_behavior_rotate_with_player = true;
+var g_ignore_view_change_events = false;
+
+function updatePlayerPositionAndRotation(lon, lat, rot, speed) {
+    var map_coords = game_coord_to_pixels(lon, lat);
+    var rad = rot * Math.PI * 2;
+
+    g_playerFeature.getGeometry().setCoordinates(map_coords);
+    g_playerIcon.setRotation(-rad);
+
+    g_ignore_view_change_events = true;
+    if (g_behavior_center_on_player) {
+
+        if (g_behavior_rotate_with_player) {
+            var height = g_map.getSize()[1];
+            var max_ahead_amount = height / 3.0 * g_map.getView().getResolution();
+
+            var amount_ahead = speed * 0.25;
+            amount_ahead = Math.max(-max_ahead_amount, Math.min(amount_ahead, max_ahead_amount));
+
+            var ahead_coords = [
+                map_coords[0] + Math.sin(-rad) * amount_ahead,
+                map_coords[1] + Math.cos(-rad) * amount_ahead
+            ];
+            g_map.getView().setCenter(ahead_coords);
+            g_map.getView().setRotation(rad);
+        } else {
+            g_map.getView().setCenter(map_coords);
+            g_map.getView().setRotation(0);
+        }
+    }
+    g_ignore_view_change_events = false;
+}
